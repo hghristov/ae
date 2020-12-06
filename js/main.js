cbaa699ba849f0197d968d6a5c3374c7b285f4f4
Hristo Hristov <hristo@hghristov.net>
2020-12-06 16:19:43 +0200

    adding footer mobile css/js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 (function() {
     mobileHeaderHandler();
+    mobileFooterHandler();
+
+
 
     function mobileHeaderHandler() {
         const button = document.querySelector('.js-nav-toggle'),
@@ -10,10 +13,22 @@
         const clickHandler = (e) => {
             e.preventDefault();
 
-            headerBottom.classList.toggle('visible');
-            button.classList.toggle('active');
+            headerBottom.classList.toggle('is--visible');
+            button.classList.toggle('is--active');
         }
 
         button.addEventListener('click', clickHandler);
+    }
+
+    function mobileFooterHandler() {
+        const toggle = document.querySelectorAll('.expander__toggle');
+
+        const clickHandler = (e) => {
+            const parent = e.target.parentNode;
+
+            parent.classList.toggle('is--visible');
+        }
+
+        toggle.forEach(el =>  el.addEventListener('click', clickHandler));
     }
 })()
